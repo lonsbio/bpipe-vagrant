00e1546551ff8f976366be59c2bd5f3bc71314f5
lonsbio <andrew.lonsdale@lonsbio.com.au>
2013-08-27 10:02:30 +1000

    Add minimal slurm install

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,7 @@
 apt-get update
 apt-get install -y git
 apt-get install -y zip
+apt-get install -y slurm-llnl
+/usr/sbin/create-munge-key
 mkdir -p /usr/local/bin/java
 mkdir -p /home/vagrant/removed
