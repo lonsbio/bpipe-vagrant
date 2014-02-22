05c58b18575704521e2d3bcb77cc9c2d3d6cf1ea
Andrew Lonsdale <andrew.lonsdale@lonsbio.com.au>
2014-02-22 18:05:48 +1100

    Cleanup old checkout

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get install -y git
 apt-get install -y zip
 apt-get install -y slurm-llnl
+apt-get install -y r-base-core
 /usr/sbin/create-munge-key
 mkdir -p /usr/local/bin/java
 mkdir -p /home/vagrant/removed
